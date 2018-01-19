12ccdce7b4a79e30727ca72e89acc7774d438f35
Lenny Truong <lenny@cs.stanford.edu>
2018-01-19 14:02:26 -0800

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='pe',
+    version='0.1-alpha',
+    description='Executable specification for the CGRA PE',
+    scripts=[],
+    packages=[
+        "pe",
+    ],
+    python_requires='>=3.6'
+)
