efc153245bce51cb516b6763e3dfa9e8930aaf33
Lenny Truong <lenny@stanford.edu>
2018-04-03 12:33:51 -0700

    Update V flag for abs

--- pe/pe.py
+++ pe/pe.py
@@ -158,7 +158,8 @@ class PE:
         elif self.opcode & 0xFF == 0x1: # sub
             V = (ra[15] != rb[15]) and (ra[15] != (ra + ~rb + 1)[15])
         elif self.opcode & 0xFF == 0x3: # abs
-            V = (~ra + 1)[15]
+            V = ra == 0x8000
+            # V = alu_res[15]
         elif self.opcode & 0xFF in [0xb, 0xc]: # mul0, mul1
             V = (ra * rb)[15] if (ra[15] == rb[15]) else (ra * rb)[15] == 0 and (ra != 0 or rb != 0)
         elif self.opcode & 0xFF == 0xd:
