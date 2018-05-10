3c88c35f1be0544cd1e3d34b08de918656514ee6
Raj Setaluri <raj.setaluri@gmail.com>
2018-05-10 11:42:21 -0700

    Do better error reporting in context_parser

--- dsl/context_parser.py
+++ dsl/context_parser.py
@@ -29,35 +29,41 @@ class ParseError(ParseStatus):
         return False
 
 
-class GenericParseError(ParseError):
-    def __init__(self, obj):
+class ParseTypeError(ParseError):
+    def __init__(self, name, valid_types, obj):
+        self.name = name
+        self.valid_types = valid_types
         self.obj = obj
 
     def __str__(self):
-        return str(self.obj)
+        actual_type_name = type(self.obj).__name__
+        valid_type_names = ", ".join([t.__name__ for t in self.valid_types])
+        return ("Expected %s to be of type %s. Got %s instead." %
+                (self.name, valid_type_names, actual_type_name))
 
 
 class ContextParser:
     @staticmethod
     def parse_literal(node):
         if not isinstance(node.get_type(), types.Type):
-            return GenericParseError("Expected literal type to be type Type")
+            return ParseTypeError("Literal.type", (types.Type,),
+                                  node.get_type())
         return ParseSuccess()
 
     @staticmethod
     def parse_variable_declaration(node):
         if not isinstance(node.get_id(), str):
-            return GenericParseError("Expected variable declaration id to be "
-                                     "type str")
+            return ParseTypeError("VariableDeclaration.id", (str,),
+                                  node.get_id())
         if not isinstance(node.get_type(), types.Type):
-            return GenericParseError("Expected variable declaration type to be "
-                                     "type Type")
+            return ParseTypeError("VariableDeclaration.type", (types.Type,),
+                                  node.get_type())
         return ParseSuccess()
 
     @staticmethod
     def parse_name(node):
         if not isinstance(node.get_id(), str):
-            return GenericParseError("Expected name id to be type str")
+            return ParseTypeError("Name.id", (str,), node.get_id())
         return ParseSuccess()
 
     @staticmethod
@@ -68,8 +74,8 @@ class ContextParser:
             if isinstance(lvalue, nodes.Expression) and \
                isinstance(lvalue.get_op(), ops.Slice):
                 return ContextParser.parse_expression(lvalue)
-            return GenericParseError("Expected lvalue of assignment to be type "
-                                     "Name or Exprssion(Slice)")
+            return ParseTypeError("Assignment.lhs",
+                                  (nodes.Name, nodes.Expression), lvalue)
 
         lvalue_status = parse_lvalue(node.get_lhs())
         if not lvalue_status.ok():
@@ -80,8 +86,7 @@ class ContextParser:
     def parse_expression(node):
         def parse_op(op):
             if not isinstance(op, ops.Op):
-                return GenericParseError("Expected op of expression to be type "
-                                         "Op")
+                return ParseTypeError("Expression.op", (ops.Op,), op)
             return ParseSuccess()
 
         if isinstance(node, nodes.Name):
@@ -100,8 +105,10 @@ class ContextParser:
         # NOTE(raj): This is somewhat of a hack for now.
         if isinstance(node, (str, int)):
             return ParseSuccess()
-        return GenericParseError("Expected expression to be type Name, "
-                                 "Literal, or Expression")
+        return ParseTypeError("Expression", (nodes.Name,
+                                             nodes.Literal,
+                                             nodes.Expression,
+                                             str, int), node)
 
     @staticmethod
     def parse_switch_case(node):
@@ -111,7 +118,7 @@ class ContextParser:
         def parse_case(case):
             if isinstance(case, nodes.Literal):
                 return ContextParser.parse_literal(case)
-            return GenericParseError("Expected case to be type Literal")
+            return ParseTypeError("SwitchCase.case", (nodes.Literal,), case)
 
         subject_status = parse_subject(node.get_subject())
         if not subject_status.ok():
@@ -133,7 +140,9 @@ class ContextParser:
             return ContextParser.parse_assignment(node)
         if isinstance(node, nodes.SwitchCase):
             return ContextParser.parse_switch_case(node)
-        return GenericParseError("Wrong type of root node")
+        return ParseTypeError("statement", (nodes.VariableDeclaration,
+                                            nodes.Assignment,
+                                            nodes.SwitchCase), node)
 
     @staticmethod
     def parse_body(body):
